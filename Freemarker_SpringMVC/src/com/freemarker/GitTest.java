8cd497fc80041ef99be933e23f3334b6d85790f0
vodinh <vodinh90@gmail.com>
2014-03-27 23:37:10 +0700

    this is change from
    Signed-off-by: vodinh <vodinh90@gmail.com>

--- Freemarker_SpringMVC/src/com/freemarker/GitTest.java
+++ Freemarker_SpringMVC/src/com/freemarker/GitTest.java
@@ -1,7 +1,15 @@
 package com.freemarker;
 
+/**
+ * Test Git
+ * @author JavaDev2
+ *
+ */
 public class GitTest {
 	public static void main(String[] args) {
+		/**
+		 * This is change from vodinh
+		 */
 		System.out.println("GIT is cool");
 	}
 }
